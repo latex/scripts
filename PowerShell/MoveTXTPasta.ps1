725eb41b61ab9550765acc45ea7c4376f021f16a
Leandro Teixeira <leandro.tex@outlook.com>
2017-06-19 16:00:47 -0300

    Teste

---
+++ PowerShell/MoveTXTPasta.ps1
@@ -0,0 +1,32 @@
+﻿#
+# Script.ps1
+#
+
+Get-ChildItem  -recurse -dir -Path ".\"| ForEach-Object{
+	 $nome = $_.BaseName;
+	 $a = $nome.ToString();
+	if( $a  -eq "txt")
+	{
+		
+		## Remove the original  root folder
+        Write-Host "Encontrado " $_.FullName
+		
+        $dest = Split-Path(Split-Path  $_.FullName -parent) -parent
+        
+        Write-Host "Movendo para" $dest
+       write-host " "
+
+        $souce = $_.FullName
+        Get-ChildItem -Filter *.txt -Path $souce | ForEach-Object{
+            Write-Host "copiando " $_.Name "para " $dest
+            Copy-Item -Filter *.txt -Force -Path $_.FullName -Destination $dest
+
+        }
+        
+		#Copy-Item -Filter *.txt -Recurse -Force -Path $_. -Destination $dest
+		#Remove-Item -Path $_.FullName -Force -Recurse 
+	}
+	#cria pasta
+	#move pasta 
+
+}
--- PowerShell/ZipFiles.ps1
+++ PowerShell/ZipFiles.ps1
@@ -3,5 +3,6 @@ function AddZip($zipfile,$sourcefile)
         
         Copy-toZip $sourcefile -ZipFile $zipfile
         Remove-item $sourcefile
+        Write-Host $sourcefile
         
     }
